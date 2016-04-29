e81442e7ed51ed5b5944d75aab7d8fb2dfcd74c6
Jessica <jessica.kalberer@gmail.com>
2016-04-29 17:45:56 +0200

    add Statistic View

--- app/src/main/java/ch/hsr/qfs/view/MainActivity.java
+++ app/src/main/java/ch/hsr/qfs/view/MainActivity.java
@@ -135,6 +135,9 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.nav_category:
                     changeFragment(new QuizCategoryFragment());
                     return true;
+                case R.id.nav_statistic:
+                    changeFragment(new QuizStatisticFragment());
+                    return true;
                 case R.id.nav_logout:
                     authService.logout(new ApiHttpCallback<ApiHttpResponse<User>>() {
                         @Override
