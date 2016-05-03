5a176c42de7d4477bb5375b3c498939fd2629ec6
Jessica <jessica.kalberer@gmail.com>
2016-05-03 16:19:22 +0200

    populate error

--- app/src/main/java/ch/hsr/qfs/view/QuizStatisticFragment.java
+++ app/src/main/java/ch/hsr/qfs/view/QuizStatisticFragment.java
@@ -30,9 +30,21 @@ public class QuizStatisticFragment extends Fragment {
     TextView tvUsername1;
     TextView tvUsername2;
 
+    Button btn_statisticQ1;
+    Button btn_statisticQ2;
+    Button btn_statisticQ3;
+    Button btn_statisticQ4;
+    Button btn_statisticQ5;
+    Button btn_statisticQ6;
+    Button btn_statisticQ7;
+    Button btn_statisticQ8;
+    Button btn_statisticQ9;
+    Button btn_statisticQ10;
+    Button btn_statisticQ11;
+    Button btn_statisticQ12;
+
     public QuizStatisticFragment() {
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -46,6 +58,19 @@ public class QuizStatisticFragment extends Fragment {
         tvUsername1 = (TextView) viewRoot.findViewById(R.id.tvUsername1);
         tvUsername2 = (TextView) viewRoot.findViewById(R.id.tvUsername2);
 
+        btn_statisticQ1 = (Button) viewRoot.findViewById(R.id.btn_statisticQ1);
+        btn_statisticQ2 = (Button) viewRoot.findViewById(R.id.btn_statisticQ2);
+        btn_statisticQ3 = (Button) viewRoot.findViewById(R.id.btn_statisticQ3);
+        btn_statisticQ4 = (Button) viewRoot.findViewById(R.id.btn_statisticQ4);
+        btn_statisticQ5 = (Button) viewRoot.findViewById(R.id.btn_statisticQ5);
+        btn_statisticQ6 = (Button) viewRoot.findViewById(R.id.btn_statisticQ6);
+        btn_statisticQ7 = (Button) viewRoot.findViewById(R.id.btn_statisticQ7);
+        btn_statisticQ8 = (Button) viewRoot.findViewById(R.id.btn_statisticQ8);
+        btn_statisticQ9 = (Button) viewRoot.findViewById(R.id.btn_statisticQ9);
+        btn_statisticQ10 = (Button) viewRoot.findViewById(R.id.btn_statisticQ10);
+        btn_statisticQ11 = (Button) viewRoot.findViewById(R.id.btn_statisticQ11);
+        btn_statisticQ12 = (Button) viewRoot.findViewById(R.id.btn_statisticQ12);
+
         qs.get(quizId, new ApiHttpCallback<ApiHttpResponse<Quiz>>() {
             @Override
             public void onCompletion(ApiHttpResponse<Quiz> response) {
@@ -57,6 +82,7 @@ public class QuizStatisticFragment extends Fragment {
                     tvUsername2.setText(opponent.getUsername());
                     Log.d("QFS", "QuizID: " + quizId);
 
+                   // if (quiz.get)
 
 
                 } else {
